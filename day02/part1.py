f330e249f3c98395fd08650f7ae2d1202cb6ef4a
Kamil Turek <kamil.turek@hotmail.com>
2023-12-03 10:39:15 +0100

    Refactor day02

--- day02/part1.py
+++ day02/part1.py
@@ -2,6 +2,23 @@ import re
 import sys
 
 
+def parse_game(game):
+    game_maxes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+
+    game_id, cube_sets = re.match(r"Game (\d+): (.*)", game).groups()
+    cube_sets = cube_sets.split(";")
+
+    for cube_set in cube_sets:
+        for count, color in re.findall("(\d+) (\w+)", cube_set):
+            game_maxes[color] = max(game_maxes[color], int(count))
+
+    return int(game_id), game_maxes
+
+
 def solve(input):
     """
     >>> solve(open('input1.txt'))
@@ -15,29 +32,11 @@ def solve(input):
         "blue": 14,
     }
 
-    total = 0
-
-    for game in input:
-        game_maxes = {
-            "red": 0,
-            "green": 0,
-            "blue": 0,
-        }
-
-        game_id, cube_sets = re.match(r"Game (\d+): (.*)", game).groups()
-        cube_sets = cube_sets.split(";")
-
-        for cube_set in cube_sets:
-            for count, color in re.findall("(\d+) (\w+)", cube_set):
-                game_maxes[color] = max(game_maxes[color], int(count))
-
-        for color, max_count in maxes.items():
-            if game_maxes[color] > max_count:
-                break
-        else:
-            total += int(game_id)
-
-    return total
+    return sum(
+        game_id
+        for game_id, game_maxes in (parse_game(line) for line in input)
+        if all(count <= maxes[color] for color, count in game_maxes.items())
+    )
 
 
 if __name__ == "__main__":
--- day02/part2.py
+++ day02/part2.py
@@ -4,6 +4,23 @@ import re
 import sys
 
 
+def parse_game(game):
+    game_maxes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+
+    cube_sets = re.match(r"Game \d+: (.*)", game).group(0)
+    cube_sets = cube_sets.split(";")
+
+    for cube_set in cube_sets:
+        for count, color in re.findall("(\d+) (\w+)", cube_set):
+            game_maxes[color] = max(game_maxes[color], int(count))
+
+    return game_maxes
+
+
 def solve(input):
     """
     >>> solve(open('input1.txt'))
@@ -11,26 +28,10 @@ def solve(input):
     >>> solve(open('input2.txt'))
     84538
     """
-    total = 0
-
-    for game in input:
-        game_maxes = {
-            "red": 0,
-            "green": 0,
-            "blue": 0,
-        }
-
-        cube_sets = re.match(r"Game \d+: (.*)", game).group(0)
-        cube_sets = cube_sets.split(";")
-
-        for cube_set in cube_sets:
-            for count, color in re.findall("(\d+) (\w+)", cube_set):
-                game_maxes[color] = max(game_maxes[color], int(count))
-
-        power = functools.reduce(operator.mul, game_maxes.values())
-        total += power
-
-    return total
+    return sum(
+        functools.reduce(operator.mul, game.values())
+        for game in (parse_game(line) for line in input)
+    )
 
 
 if __name__ == "__main__":
